1377e73cd55830bd85c06e0310b74e3b7570c5d6
Martin Persson <martin.persson@physics.gu.se>
2011-01-17 19:36:58 +0100

    Changed "Foobar" to "GenerateHologramCUDA"

--- source/GenerateHologramCUDA.cu
+++ source/GenerateHologramCUDA.cu
@@ -18,7 +18,7 @@
     GNU General Public License for more details.
 
     You should have received a copy of the GNU General Public License
-    along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+    along with GenerateHologramCUDA.  If not, see <http://www.gnu.org/licenses/>.
 */
 ///////////////////////////////////////////////////////////////////////////////////
 //The function "GenerateHologram" contains two different algorithms for
--- source/source DFT/computeDelta.cu
+++ source/source DFT/computeDelta.cu
@@ -17,7 +17,7 @@
     GNU General Public License for more details.
 
     You should have received a copy of the GNU General Public License
-    along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+    along with GenerateHologramCUDA.  If not, see <http://www.gnu.org/licenses/>.
 */
 ///////////////////////////////////////////////////////////////////////////////////////
 //Calculate one matrix for each spot containing its distance to each pixel on the SLM
