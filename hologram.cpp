6979f62412f9d42558cf935f9590c8ff4b066a70
Samuel Grayson <sam@samgrayson.me>
2020-06-14 14:43:50 -0500

    Use threadloop

--- hologram.cpp
+++ hologram.cpp
@@ -27,17 +27,18 @@ public:
 		, _seq_expect(1)
 		, _stat_processed(0)
 		, _stat_missed(0)
+		, logger{"hologram"}
 	{
-		logger = new start_end_logger("hologram");
+		auto ret = HLG_initailize();
+		throw std::runtime_error{"Hologram Initialization failed (" + std::to_string(ret) + ")"};
 	}
 
-	void main_loop() {
-		while (!_m_terminate.load()) {
+	void _p_one_iteration() override {
 			auto in = _m_in->get_latest_ro();
 			if (!in || in->seq == _seq_expect-1) {
 				// No new data, sleep
 				std::this_thread::yield(); // ←_←
-				continue;
+				return;
 			}
 			if (in->seq != _seq_expect) {
 				_stat_missed = in->seq - _seq_expect;
@@ -46,34 +47,18 @@ public:
 			}
 			_stat_processed++;
 			//printf("[Hologram] Running sample %ld, samples dropped since last sample: %ld\n", _stat_processed, _stat_missed);
-			logger->log_start(std::chrono::high_resolution_clock::now());
+			logger.log_start(std::chrono::high_resolution_clock::now());
 			HLG_process();
-			logger->log_end(std::chrono::high_resolution_clock::now());
+			logger.log_end(std::chrono::high_resolution_clock::now());
 			_seq_expect = in->seq+1;
-		}
-	}
-
-	virtual void start() override {
-		if (!HLG_initailize()) {
-			fprintf(stderr, "Hologram Initialization failed\n");
-			exit(1);
-		}
-		_m_thread = thread{&hologram::main_loop, this};
-	}
-
-	virtual void _p_stop() {
-		_m_terminate.store(true);
-		_m_thread.join();
 	}
 
 private:
 	switchboard *sb;
-	start_end_logger* logger;
 	unique_ptr<reader_latest<hologram_input>> _m_in;
 	unique_ptr<writer<hologram_output>> _m_out;
-	thread _m_thread;
-	atomic<bool> _m_terminate {false};
 	long long _seq_expect, _stat_processed, _stat_missed;
+	start_end_logger logger;
 
 };
 
