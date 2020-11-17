4fc6c2dae938fb525973ddf50cd41dde9a2706ea
Muhammad Huzaifa <huzaifa2@illinois.edu>
2020-11-17 15:39:48 -0600

    Do dos2unix

--- C/source/testMain.cu
+++ C/source/testMain.cu
@@ -23,12 +23,16 @@
 
 int main(int argc, char *argv[])
 {
-  HLG_initialize();
+    if (argc != 2) {
+        printf("Usage: ./hologram <num trials>\n");
+        return 0;
+    }
 
-  for (int i = 0; i < 10; ++i) {
-    HLG_process();
-  }
+    unsigned trials = atoi(argv[1]);
 
-  HLG_cleanup();
-  return 0;
+    HLG_initialize();
+    for (unsigned i = 0; i < trials; i++)
+      HLG_process();
+    HLG_cleanup();
+    return 0;
 }
