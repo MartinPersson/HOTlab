13d788e45d0d6e08bbd2dbe7c80110c75b4afe8d
Giordano Salvador <73959795+e3m3@users.noreply.github.com>
2021-02-23 14:19:56 -0500

    Integrated with new Switchboard API (#3)
    * Integrating with new Switchboard API
    
    * Removed link to common
    
    * Adding back _p_should_skip (#2)
    
    * Removed unused hologram output (#2)

--- hologram.cpp
+++ hologram.cpp
@@ -21,34 +21,32 @@ using std::atomic;
 
 class hologram : public threadloop {
 public:
-	hologram(std::string name_, phonebook* pb_)
-		: threadloop{name_, pb_}
-		, sb{pb->lookup_impl<switchboard>()}
-		, _m_in{sb->subscribe_latest<hologram_input>("hologram_in")}
-		, _m_out{sb->publish<hologram_output>("hologram_out")}
-		, _seq_expect(1)
-		, _stat_processed(0)
-		, _stat_missed(0)
-		, logger{"hologram"}
-	{
-		bool ret = HLG_initialize();
-		if (!ret) {
-			throw std::runtime_error{"Hologram Initialization failed (" + std::to_string(ret) + ")"};
-		}
+    hologram(std::string name_, phonebook* pb_)
+        : threadloop{name_, pb_}
+        , sb{pb->lookup_impl<switchboard>()}
+        , _m_in{sb->get_reader<hologram_input>("hologram_in")}
+        , _seq_expect{1}
+        , _stat_processed{0}
+        , _stat_missed{0}
+    {
+        bool ret = HLG_initialize();
+        if (!ret) {
+            throw std::runtime_error{"Hologram Initialization failed (" + std::to_string(ret) + ")"};
+        }
 
-		cudaEventCreate(&_start);
-		cudaEventCreate(&_stop);
-	}
+        cudaEventCreate(&_start);
+        cudaEventCreate(&_stop);
+    }
 
-	// destructor
-	virtual ~hologram() override {
-		for (int i = 0; i < _start_durations.size(); ++i) {
-			std::cout << "gpu_timer,hologram," << i << ",0,0," << (_stop_durations[i] - _start_durations[i]) * 1000000 << "\n";
-		}
-	}
+    // destructor
+    virtual ~hologram() override {
+        for (int i = 0; i < _start_durations.size(); ++i) {
+            std::cout << "gpu_timer,hologram," << i << ",0,0," << (_stop_durations[i] - _start_durations[i]) * 1000000 << "\n";
+        }
+    }
 
-	virtual skip_option _p_should_skip() override {
-		auto in = _m_in->get_latest_ro();
+    virtual skip_option _p_should_skip() override {
+		auto in = _m_in.get_ro_nullable();
 		if (!in || in->seq == _seq_expect-1) {
 			// No new data, sleep
 			return skip_option::skip_and_yield;
@@ -64,34 +62,34 @@ public:
 		}
 	}
 
-	void _p_one_iteration() override {
-		_start_durations.push_back(_total_gpu_time);
+    void _p_one_iteration() override {
+        _start_durations.push_back(_total_gpu_time);
 
-		cudaEventRecord(_start, 0);
-		HLG_process();
-		cudaEventRecord(_stop, 0);
+        cudaEventRecord(_start, 0);
+        HLG_process();
+        cudaEventRecord(_stop, 0);
 
-		cudaEventSynchronize(_stop);
-    float elapsed_time;
-    cudaEventElapsedTime(&elapsed_time, _start, _stop);
-    _total_gpu_time += elapsed_time;
+        cudaEventSynchronize(_stop);
+        float elapsed_time;
+        cudaEventElapsedTime(&elapsed_time, _start, _stop);
+        _total_gpu_time += elapsed_time;
 
-		_stop_durations.push_back(_total_gpu_time);
-	}
+        _stop_durations.push_back(_total_gpu_time);
+    }
 
 private:
-	const std::shared_ptr<switchboard> sb;
-	unique_ptr<reader_latest<hologram_input>> _m_in;
-	unique_ptr<writer<hologram_output>> _m_out;
-	long long _seq_expect, _stat_processed, _stat_missed;
-	start_end_logger logger;
+    const std::shared_ptr<switchboard> sb;
+    switchboard::reader<hologram_input> _m_in;
+    long long _seq_expect;
+    long long _stat_processed;
+    long long _stat_missed;
 
-	// Timing
-	cudaEvent_t _start;
-	cudaEvent_t _stop;
-	std::vector<float> _start_durations;
-	std::vector<float> _stop_durations;
-	float _total_gpu_time = 0;
+    // Timing
+    cudaEvent_t _start;
+    cudaEvent_t _stop;
+    std::vector<float> _start_durations;
+    std::vector<float> _stop_durations;
+    float _total_gpu_time = 0;
 };
 
 
